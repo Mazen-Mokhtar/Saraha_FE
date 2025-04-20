ea4e02018f0e22828de1c050eeb83a2d64a50874
Mazen Mokhtar <mazenmokhtar.iq@gmail.com>
2025-04-20 07:32:41 +0200

    fix url

--- app/lib/axios.ts
+++ app/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://saraha-app-production-317b.up.railway.app',
+  baseURL: 'https://saraha-app-production-317b.up.railway.app',
   headers: {
     'Content-Type': 'application/json',
   },
